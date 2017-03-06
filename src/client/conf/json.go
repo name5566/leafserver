7d0f909eb1cebbea15d37dace027dec16660dd3e
heyilin <240348182@163.com>
2017-03-06 17:02:31 +0800

    忘提交client/conf文件夹了；

---
+++ src/client/conf/json.go
@@ -0,0 +1,34 @@
+package conf
+
+import (
+	"encoding/json"
+	"github.com/name5566/leaf/log"
+	"io/ioutil"
+)
+
+var Server struct {
+	LogLevel    	string
+	LogPath     	string
+	WSAddr      	string
+	CertFile    	string
+	KeyFile     	string
+	TCPAddr     	string
+	MaxConnNum  	int
+	ConsolePort 	int
+	ProfilePath 	string
+	ServerName		string
+	ListenAddr     	string
+	ConnAddrs       []string
+	PendingWriteNum int
+}
+
+func init() {
+	data, err := ioutil.ReadFile("conf/client.json")
+	if err != nil {
+		log.Fatal("%v", err)
+	}
+	err = json.Unmarshal(data, &Server)
+	if err != nil {
+		log.Fatal("%v", err)
+	}
+}
